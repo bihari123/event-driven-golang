421d787544f053cd463fd4e19e8318b408d802b2
Bihari123 <thakur.cs.tarun@gmail.com>
2024-08-31 08:26:39 +0530

    add: middleware

--- project/exercise.md
+++ project/exercise.md
@@ -1,152 +1,71 @@
-# Project: Payloads
+# Project: Logging middleware
 
+Some middleware functions need a dependency or config. There are two approaches you can use.
 
-<div class="alert alert-dismissible bg-info text-white d-flex flex-column flex-sm-row p-7 mb-10">
-    <div class="d-flex flex-column">
-        <h3 class="mb-5 text-white">
-			Background	
-		</h3>
-        <span>
+First, simply pass the dependency as an argument to the middleware.
+It looks quite complex, but it's basically a function returning a middleware function.
 
-Our integrations work correctly, but adding just the ticket IDs isn't that helpful because the operations team still has to manually look up the ticket details.
-We can help them out by adding more details to the external systems.
+```go
+func SaveMessageMiddleware(saver MessageSaver) func (h message.HandlerFunc) message.HandlerFunc {
+	return func(next message.HandlerFunc) message.HandlerFunc {
+		return func(msg *message.Message) ([]*message.Message, error) {
+			err := saver.Save(msg)
+			if err != nil {
+				return nil, err
+			}
+			
+			return next(msg)
+		}
+	}
+}
+```
 
-</span>
-	</div>
-	</div>
+Use it like this:
+
+```go
+router.AddMiddleware(SaveMessageMiddleware(saver))
+```
+
+When you need more dependencies, you can consider making the function a struct.
+
+```go
+type RandomFail struct {
+	Chance float64
+	Error error
+}
+
+func (m RandomFail) Middleware(next message.HandlerFunc) message.HandlerFunc {
+	return func(msg *message.Message) ([]*message.Message, error) {
+		if rand.Float64() < m.Chance {
+			return nil, m.Error
+		}
+		return next(msg)
+	}
+}
+```
+
+Use it like this (notice how the `Middleware` method is passed, not the struct itself):
+
+```go
+router.AddMiddleware(RandomFail{
+	Chance: 0.1, 
+	Error: errors.New("random error occurred"),
+}.Middleware)
+```
 
 ## Exercise
 
 File: `project/main.go`
 
-First, we're going to extend the webhook endpoint to accept more than ticket IDs.
-We're dropping support for `POST /tickets-confirmation`.
-Instead, we're now going to expose `POST /tickets-status`.
+Add a middleware function to your project that logs incoming messages. The log message should be:
 
-Previously, the endpoint accepted a list of ticket IDs; now it's going to accept a list of tickets.
-Each ticket has a ticket ID, a status, a customer email, and a price.
-
-Here's an example incoming HTTP request:
-
-```json
-{
-  "tickets": [
-    {
-      "ticket_id": "ticket-1",
-      "status": "confirmed",
-      "customer_email": "user@example.com",
-      "price": {
-        "amount": "50.00",
-        "currency": "EUR"
-      }
-    }
-  ]
-}
+```text
+Handling a message
 ```
 
-
-<div class="alert alert-dismissible bg-light-primary d-flex flex-column flex-sm-row p-7 mb-10">
-    <div class="d-flex flex-column">
-        <h3 class="mb-5 text-dark">
-			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightbulb text-primary" viewBox="0 0 16 16">
-			  <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1z"/>
-			</svg>
-			Tip
-		</h3>
-        <span>
-
-Note that we use `string` types for the price amount.
-This is on purpose.
-Don't use `float64` for money, or you may lose precision.
-
-</span>
-	</div>
-	</div>
-
-The endpoint should still publish two messages per ticket: one for issuing a receipt and one for appending it to the spreadsheet.
-However, we want to send more details to both external systems.
-
-For issuing the receipts, we need to include the price:
+It should include a log field called `message_uuid` with the message UUID.
+Using logrus, you can add log fields like this:
 
 ```go
-body := receipts.PutReceiptsJSONRequestBody{
-	TicketId: request.TicketID, 
-	Price: receipts.Money{
-		MoneyAmount:   request.Price.Amount, 
-		MoneyCurrency: request.Price.Currency,
-	},
-}
-```
-
-
-<div class="alert alert-dismissible bg-light-primary d-flex flex-column flex-sm-row p-7 mb-10">
-    <div class="d-flex flex-column">
-        <h3 class="mb-5 text-dark">
-			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightbulb text-primary" viewBox="0 0 16 16">
-			  <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1z"/>
-			</svg>
-			Tip
-		</h3>
-        <span>
-
-Note that there are two `Money` types: one comes from the HTTP request, and the other from the `receipts` package.
-This is correct.
-They come from different contexts, and you need to map one to the other.
-
-</span>
-	</div>
-	</div>
-
-And for each row appended to the sheet, we're going to add the customer's email and the price:
-
-```go
-spreadsheetsClient.AppendRow(
-	msg.Context(), 
-	"tickets-to-print", 
-	[]string{payload.TicketID, payload.CustomerEmail, payload.Price.Amount, payload.Price.Currency},
-)
-```
-
-To make this work, we need to publish proper JSON message payloads to the Pub/Sub topics.
-
-The `IssueReceiptPayload` looks like this:
-
-```json
-{
-  "ticket_id": "ticket-1",
-  "price": {
-    "amount": "50.00",
-    "currency": "EUR"
-  }
-}
-```
-
-And the `AppendToTrackerPayload` like this:
-
-```json
-{
-  "ticket_id": "ticket-1",
-  "customer_email": "user@example.com",
-  "price": {
-    "amount": "50.00",
-    "currency": "EUR"
-  }
-}
-```
-
-Create structs for these payloads. 
-In the HTTP handler, marshal them to JSON and publish messages with them on the topics.
-
-Change the Router handlers so that they unmarshal the payload on the structs.
-Then use the values from the structs to call the external systems.
-
-Modify the receipts client so it accepts a struct as an argument:
-
-```go
-type IssueReceiptRequest struct {
-	TicketID string
-	Price    Money
-}
-
-func (c ReceiptsClient) IssueReceipt(ctx context.Context, request IssueReceiptRequest) error {
+logrus.WithField("key", value).Info("Log message")
 ```
--- project/main.go
+++ project/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/ThreeDotsLabs/go-event-driven/common/clients"
 	"github.com/ThreeDotsLabs/go-event-driven/common/clients/receipts"
@@ -15,14 +17,90 @@ import (
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
 	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/redis/go-redis/v9"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sync/errgroup"
 )
 
+/*
+```json
+{
+  "header": {
+    "id": "...",
+    "published_at": "..."
+  },
+  "ticket_id": "...",
+  "customer_email": "...",
+  "price": {
+    "amount": "100",
+    "currency": "EUR"
+  }
+}
+```
+
+*/
+
+type EventHeader struct {
+	Id          string    `json:"id"`
+	PublishedAt time.Time `json:"published_at"`
+}
+
+func NewEventHeader() EventHeader {
+	return EventHeader{
+		Id: uuid.NewString(),
+		// PublishedAt: time.Now().Format(time.RFC3339),
+		PublishedAt: time.Now().UTC(),
+	}
+}
+
+type Ticket struct {
+	TicketId      string `json:"ticket_id"`
+	Status        string `json:"status"`
+	CustomerEmail string `json:"customer_email"`
+	Price         struct {
+		Amount   string `json:"amount"`
+		Currency string `json:"currency"`
+	} `json:"price"`
+}
+
+type TicketBookingConfirmed struct {
+	Header        EventHeader `json:"header"`
+	TicketId      string      `json:"ticket_id"`
+	CustomerEmail string      `json:"customer_email"`
+	Price         struct {
+		Amount   string `json:"amount"`
+		Currency string `json:"currency"`
+	} `json:"price"`
+}
+
+type TicketIssue struct {
+	TicketId string `json:"ticket_id"`
+	Price    struct {
+		Amount   string `json:"amount"`
+		Currency string `json:"currency"`
+	} `json:"price"`
+}
+type TicketBookingCanceled struct {
+	Header        EventHeader `json:"header"`
+	TicketID      string      `json:"ticket_id"`
+	CustomerEmail string      `json:"customer_email"`
+	Price         Money       `json:"price"`
+}
+
+type Money struct {
+	Amount   string `json:"amount"`
+	Currency string `json:"currency"`
+}
+type TicketSpreadSheet struct {
+	TicketId      string `json:"ticket_id"`
+	CustomerEmail string `json:"customer_email"`
+	Price         Money  `json:"price"`
+}
+
 type TicketsConfirmationRequest struct {
-	Tickets []string `json:"tickets"`
+	Tickets []Ticket `json:"tickets"`
 }
 
 func main() {
@@ -49,6 +127,14 @@ func main() {
 		panic(err)
 	}
 
+	refundSub, err := redisstream.NewSubscriber(redisstream.SubscriberConfig{
+		Client:        rdb,
+		ConsumerGroup: "refundGroup",
+	}, logger)
+	if err != nil {
+		panic(err)
+	}
+
 	trackerSub, err := redisstream.NewSubscriber(redisstream.SubscriberConfig{
 		Client:        rdb,
 		ConsumerGroup: "trackerGroup",
@@ -64,7 +150,7 @@ func main() {
 
 	router.AddNoPublisherHandler(
 		"my_handler",
-		"issue-receipt",
+		"TicketBookingConfirmed",
 		issueSub,
 		func(msg *message.Message) error {
 			clients, err := clients.NewClients(os.Getenv("GATEWAY_ADDR"), nil)
@@ -74,16 +160,16 @@ func main() {
 			}
 			receiptsClient := NewReceiptsClient(clients)
 
-			// for msg := range messages {
-			ticketId := string(msg.Payload)
-			err = receiptsClient.IssueReceipt(msg.Context(), ticketId)
-			// }
+			var ticket TicketBookingConfirmed
+			if err := json.Unmarshal(msg.Payload, &ticket); err == nil {
+				err = receiptsClient.IssueReceipt(msg.Context(), ticket)
+			}
 			return err
 		},
 	)
 	router.AddNoPublisherHandler(
 		"my_handler2",
-		"append-to-tracker",
+		"TicketBookingConfirmed",
 		trackerSub,
 		func(msg *message.Message) error {
 			clients, err := clients.NewClients(os.Getenv("GATEWAY_ADDR"), nil)
@@ -94,19 +180,56 @@ func main() {
 			spreadsheetClient := NewSpreadsheetsClient(clients)
 
 			// for msg := range messages {
-			ticketId := string(msg.Payload)
-			err = spreadsheetClient.AppendRow(
-				msg.Context(),
-				"tickets-to-print",
-				[]string{ticketId},
-			)
+
+			var ticket TicketBookingConfirmed
+			if err := json.Unmarshal(msg.Payload, &ticket); err == nil {
+				err = spreadsheetClient.AppendRow(
+					msg.Context(),
+					"tickets-to-print",
+					[]string{
+						ticket.TicketId,
+						ticket.CustomerEmail,
+						ticket.Price.Amount,
+						ticket.Price.Currency,
+					},
+				)
+			}
+			return err
+		},
+	)
+
+	router.AddNoPublisherHandler(
+		"my_handler3",
+		"TicketBookingCanceled",
+		refundSub,
+		func(msg *message.Message) error {
+			clients, err := clients.NewClients(os.Getenv("GATEWAY_ADDR"), nil)
+			if err != nil {
+				// panic(err)
+				return err
+			}
+			spreadsheetClient := NewSpreadsheetsClient(clients)
+
+			var ticket TicketBookingCanceled
+			if err := json.Unmarshal(msg.Payload, &ticket); err == nil {
+				err = spreadsheetClient.AppendRow(
+					msg.Context(),
+					"tickets-to-refund",
+					[]string{
+						ticket.TicketID,
+						ticket.CustomerEmail,
+						ticket.Price.Amount,
+						ticket.Price.Currency,
+					},
+				)
+			}
 			return err
 		},
 	)
 
 	e := commonHTTP.NewEcho()
 
-	e.POST("/tickets-confirmation", func(c echo.Context) error {
+	e.POST("/tickets-status", func(c echo.Context) error {
 		var request TicketsConfirmationRequest
 		err := c.Bind(&request)
 		if err != nil {
@@ -114,31 +237,57 @@ func main() {
 		}
 
 		for _, ticket := range request.Tickets {
-			// w.Send(Message{Task: TaskIssueReceipt, TicketId: ticket},
-			// 	Message{Task: TaskAppendToTracker, TicketId: ticket},
-			// )
-
-			go func() {
-				for {
-					msg := message.NewMessage(watermill.NewUUID(), []byte(string(ticket)))
-					if err := publisher.Publish("issue-receipt", msg); err != nil {
-						continue
-					}
-					break
+			if ticket.Status == "confirmed" {
+				event := TicketBookingConfirmed{
+					TicketId:      ticket.TicketId,
+					CustomerEmail: ticket.CustomerEmail,
+					Price:         ticket.Price,
+					Header:        NewEventHeader(),
 				}
-			}()
+				if jsonObj, err := json.Marshal(event); err == nil {
 
-			go func(ticketId []byte) {
-				for {
-					msg := message.NewMessage(watermill.NewUUID(), ticketId)
-					if err := publisher.Publish("append-to-tracker", msg); err != nil {
-						continue
-					}
-					break
+					go func(ticket []byte) {
+						for {
+
+							msg := message.NewMessage(watermill.NewUUID(), ticket)
+							if err := publisher.Publish("TicketBookingConfirmed", msg); err != nil {
+								continue
+							}
+							break
+						}
+					}(jsonObj)
+
+					go func(ticket []byte) {
+						for {
+							msg := message.NewMessage(watermill.NewUUID(), ticket)
+							if err := publisher.Publish("TicketBookingConfirmed", msg); err != nil {
+								continue
+							}
+							break
+						}
+					}(jsonObj)
 				}
-			}([]byte(string(ticket)))
+
+			} else {
+				event := TicketBookingCanceled{
+					TicketID:      ticket.TicketId,
+					CustomerEmail: ticket.CustomerEmail,
+					Price:         ticket.Price,
+					Header:        NewEventHeader(),
+				}
+				if jsonObj, err := json.Marshal(event); err == nil {
+					go func(ticket []byte) {
+						for {
+							msg := message.NewMessage(watermill.NewUUID(), ticket)
+							if err := publisher.Publish("TicketBookingCanceled", msg); err != nil {
+								continue
+							}
+							break
+						}
+					}(jsonObj)
+				}
+			}
 		}
-
 		return c.NoContent(http.StatusOK)
 	})
 
@@ -185,9 +334,13 @@ func NewReceiptsClient(clients *clients.Clients) ReceiptsClient {
 	}
 }
 
-func (c ReceiptsClient) IssueReceipt(ctx context.Context, ticketID string) error {
+func (c ReceiptsClient) IssueReceipt(ctx context.Context, ticket TicketBookingConfirmed) error {
 	body := receipts.PutReceiptsJSONRequestBody{
-		TicketId: ticketID,
+		TicketId: ticket.TicketId,
+		Price: receipts.Money{
+			MoneyAmount:   ticket.Price.Amount,
+			MoneyCurrency: ticket.Price.Currency,
+		},
 	}
 
 	receiptsResp, err := c.clients.Receipts.PutReceiptsWithResponse(ctx, body)
